192121b774f33f995be73f0e2a036e4221d693b3
S0Ulle33 <29702137+S0Ulle33@users.noreply.github.com>
2019-08-17 21:18:12 +0300

    Update telegram_message_cleaner.py
    Simplified version, without stats and prettytable

--- telegram_message_cleaner.py
+++ telegram_message_cleaner.py
@@ -1,147 +1,200 @@
-import argparse
 import asyncio
+import os
 import sys
+import time
+from getpass import getpass
 
-from prettytable import PrettyTable
-from telethon import TelegramClient
+from telethon import TelegramClient, events
+from telethon.errors import SessionPasswordNeededError
+from telethon.network import ConnectionTcpAbridged
+from telethon.utils import get_display_name
 
-# Use your own values here
-api_id = 12345
-api_hash = '0123456789abcdef0123456789abcdef'
-client = TelegramClient('Telegram Message Cleaner', api_id, api_hash)
-
-
-async def get_info(chat):
-
-    messages = (
-        await client.get_messages(chat.entity, limit=None, from_user='me') if hasattr(chat, 'entity')
-        else await client.get_messages(chat, limit=None, from_user='me')
-    )
-
-    id_ = chat.id
-    name = chat.title
-    total_messages = messages.total
-    return id_, name, total_messages
+# Create a global variable to hold the loop we will be using
+loop = asyncio.get_event_loop()
 
 
-async def group_messages_statistic(ids):
-
-    info = []
-    await client.start()
-
-    dialogs = await client.get_dialogs()
-    groups = (
-        [await client.get_entity(int(dialog_id)) for dialog_id in ids] if ids
-        else [dialog for dialog in dialogs if dialog.is_group]
-    )
-
-    for future in asyncio.as_completed(map(get_info, groups)):
-        id_, name, total_messages = await asyncio.shield(future)
-        info.append((str(id_).lstrip("-"), name, total_messages))
-
-    return info
+def sprint(string, *args, **kwargs):
+    """Safe Print (handle UnicodeEncodeErrors on some terminals)"""
+    try:
+        print(string, *args, **kwargs)
+    except UnicodeEncodeError:
+        string = string.encode('utf-8', errors='ignore')\
+                       .decode('ascii', errors='ignore')
+        print(string, *args, **kwargs)
 
 
-def get_group_messages_statistic(ids):
-
-    loop = asyncio.get_event_loop()
-    stats = loop.run_until_complete(group_messages_statistic(ids))
-
-    table = PrettyTable()
-    table.field_names = ['ID', 'Group', 'Messages']
-    table.align['ID'] = 'c'
-    table.align['Group'] = 'c'
-    table.align['Messages'] = 'l'
-    table.sortby = 'Messages'
-    table.reversesort = True
-
-    for group in stats:
-        if group[2] == 0:
-            continue
-        table.add_row(list(group))
-
-    print(table)
+def print_title(title):
+    """Helper function to print titles to the console more nicely"""
+    sprint('\n')
+    sprint('=={}=='.format('=' * len(title)))
+    sprint('= {} ='.format(title))
+    sprint('=={}=='.format('=' * len(title)))
 
 
-async def delete_messages(ids):
-
-    await client.start()
-    await client.get_dialogs()
-
-    for id_ in ids:
-        chat = await client.get_entity(int(id_))
-        messages = await client.get_messages(chat, limit=None, from_user='me')
-
-        for message in messages:
-            await message.delete()
+async def async_input(prompt):
+    """
+    Python's ``input()`` is blocking, which means the event loop we set
+    above can't be running while we're blocking there. This method will
+    let the loop run while we wait for input.
+    """
+    print(prompt, end='', flush=True)
+    return (await loop.run_in_executor(None, sys.stdin.readline)).rstrip()
 
 
-def delete(ids):
+def get_env(name, message, cast=str):
+    """Helper to get environment variables interactively"""
+    if name in os.environ:
+        return os.environ[name]
+    while True:
+        value = input(message)
+        try:
+            return cast(value)
+        except ValueError as e:
+            print(e, file=sys.stderr)
+            time.sleep(1)
 
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(delete_messages(ids))
+
+class TelegramMessageCleaner(TelegramClient):
+    """Full featured Telegram client, meant to be used on an interactive
+       session to see what Telethon is capable off -
+
+       This client allows the user to perform some basic interaction with
+       Telegram through Telethon, such as listing dialogs (open chats),
+       talking to people, downloading media, and receiving updates.
+    """
+
+    def __init__(self, session_user_id, api_id, api_hash,
+                 proxy=None):
+        """
+        Initializes the TelegramMessageCleaner.
+        :param session_user_id: Name of the *.session file.
+        :param api_id: Telegram's api_id acquired through my.telegram.org.
+        :param api_hash: Telegram's api_hash.
+        :param proxy: Optional proxy tuple/dictionary.
+        """
+        print_title('Initialization')
+
+        print('Initializing Telegram Message Cleaner...')
+
+        # The first step is to initialize the TelegramClient, as we are
+        # subclassing it, we need to call super().__init__(). On a more
+        # normal case you would want 'client = TelegramClient(...)'
+        super().__init__(
+            # These parameters should be passed always, session name and API
+            session_user_id, api_id, api_hash,
+
+            # You can optionally change the connection mode by passing a
+            # type or an instance of it. This changes how the sent packets
+            # look (low-level concept you normally shouldn't worry about).
+            # Default is ConnectionTcpFull, smallest is ConnectionTcpAbridged.
+            connection=ConnectionTcpAbridged,
+
+            # If you're using a proxy, set it here.
+            proxy=proxy
+        )
+
+        # Calling .connect() may raise a connection error False, so you need
+        # to except those before continuing. Otherwise you may want to retry
+        # as done here.
+        print('Connecting to Telegram servers...')
+        try:
+            loop.run_until_complete(self.connect())
+        except IOError:
+            # We handle IOError and not ConnectionError because
+            # PySocks' errors do not subclass ConnectionError
+            # (so this will work with and without proxies).
+            print('Initial connection failed. Retrying...')
+            loop.run_until_complete(self.connect())
+
+        # If the user hasn't called .sign_in() or .sign_up() yet, they won't
+        # be authorized. The first thing you must do is authorize. Calling
+        # .sign_in() should only be done once as the information is saved on
+        # the *.session file so you don't need to enter the code every time.
+        if not loop.run_until_complete(self.is_user_authorized()):
+            print('First run. Sending code request...')
+            user_phone = input('Enter your phone: ')
+            loop.run_until_complete(self.sign_in(user_phone))
+
+            self_user = None
+            while self_user is None:
+                code = input('Enter the code you just received: ')
+                try:
+                    self_user =\
+                        loop.run_until_complete(self.sign_in(code=code))
+
+                # Two-step verification may be enabled, and .sign_in will
+                # raise this error. If that's the case ask for the password.
+                # Note that getpass() may not work on PyCharm due to a bug,
+                # if that's the case simply change it for input().
+                except SessionPasswordNeededError:
+                    pw = getpass('Two step verification is enabled. '
+                                 'Please enter your password: ')
+
+                    self_user =\
+                        loop.run_until_complete(self.sign_in(password=pw))
+
+    async def run(self):
+        """Main loop of the TelegramClient, will wait for user action"""
+
+        # Enter a while loop to chat as long as the user wants
+        while True:
+            # Retrieve the top dialogs. You can set the limit to None to
+            # retrieve all of them if you wish, but beware that may take
+            # a long time if you have hundreds of them.
+            dialog_count = None
+
+            # Entities represent the user, chat or channel
+            # corresponding to the dialog on the same index.
+            dialogs = await self.get_dialogs(limit=dialog_count)
+
+            i = None
+            while i is None:
+                print_title('Dialogs window')
+
+                # Display them so the user can choose
+                for i, dialog in enumerate(dialogs, start=1):
+                    if dialog.is_group:
+                        sprint('{}. {}'.format(i, get_display_name(dialog.entity)))
+
+                # Let the user decide who they want to talk to
+                print()
+                print('> From which group do you want to delete messages?')
+                print('> Available commands:')
+                print('  !q: Quits the dialogs window and exits.')
+                print('  !l: Logs out, terminating this session.')
+                print()
+                i = await async_input('Enter dialog ID or a command: ')
+                if i == '!q':
+                    return
+                if i == '!l':
+                    # Logging out will cause the user to need to reenter the
+                    # code next time they want to use the library, and will
+                    # also delete the *.session file off the filesystem.
+                    #
+                    # This is not the same as simply calling .disconnect(),
+                    # which simply shuts down everything gracefully.
+                    await self.log_out()
+                    return
+
+                try:
+                    i = int(i if i else 0) - 1
+                except ValueError:
+                    i = None
+
+            # Retrieve the selected group
+            entity = dialogs[i].entity
+
+            # Retrieve the messages
+            messages = await self.get_messages(entity, limit=None, from_user='me')
+
+            # Iterate over all and delete
+            for message in messages:
+                await message.delete()
 
 
 if __name__ == '__main__':
-
-    parser = argparse.ArgumentParser(
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-        usage='%(prog)s [-s [id id ...]] [-d [id id ...]]',
-        description='Show stats for your messages in all/specified group chats\nor delete all your messages in specified group chat(-s).',
-        epilog="""\
-Examples:
-
-1.Show stats for all your group chats:
-
-    ./telegram_message_cleaner.py -s
-
-    +------------+---------------+----------+
-    |     ID     |     Group     | Messages |
-    +------------+---------------+----------+
-    | 1300197304 |   Work Group  | 234      |
-    | 1210787553 | Bob and Alice | 75       |
-    | 1274948231 |    Project    | 68       |
-    +------------+---------------+----------+
-
-2.Show stats only for 'Work Group' chat:
-
-    ./telegram_message_cleaner.py -s 1300197304
-
-    +------------+---------------+----------+
-    |     ID     |     Group     | Messages |
-    +------------+---------------+----------+
-    | 1300197304 |   Work Group  | 234      |
-    +------------+---------------+----------+
-
-3.Show stats only for 'Bob and Alice' and 'Project' chats:
-
-    ./telegram_message_cleaner.py -s 1210787553 1274948231
-
-    +------------+---------------+----------+
-    |     ID     |     Group     | Messages |
-    +------------+---------------+----------+
-    | 1210787553 | Bob and Alice | 75       |
-    | 1274948231 |    Project    | 68       |
-    +------------+---------------+----------+
-
-4.Delete messages from 'Bob and Alice' chat:
-
-    ./telegram_message_cleaner.py -d 1210787553
-
-5.Delete messages from 'Bob and Alice', 'Project' chats:
-
-    ./telegram_message_cleaner.py -d 1210787553 1274948231""")
-    parser.add_argument('-s', '--statistic', nargs='*', default=None,
-                        metavar='id', help='if you do not specify ID as an argument, it will show stats of your messages for all group chats')
-    parser.add_argument('-d', '--delete', nargs='*', default=None,
-                        metavar='id', help='deletes all your messages from the specified group chat ID(-s)')
-
-    if len(sys.argv) <= 1:
-        parser.print_help()
-        sys.exit(1)
-
-    args = parser.parse_args()
-    if args.statistic or args.statistic == []:
-        get_group_messages_statistic(args.statistic)
-    elif args.delete:
-        delete(args.delete)
+    SESSION = os.environ.get('TG_SESSION', 'Telegram Message Cleaner')
+    API_ID = get_env('TG_API_ID', 'Enter your API ID: ', int)
+    API_HASH = get_env('TG_API_HASH', 'Enter your API hash: ')
+    client = TelegramMessageCleaner(SESSION, API_ID, API_HASH)
+    loop.run_until_complete(client.run())
